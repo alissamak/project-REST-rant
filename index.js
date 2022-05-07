913d5a65300e7ec20eeae9cb7f68280342155ddb
Alissa <alissamak@outlook.com>
2022-05-07 13:50:55 -0400

    added erro404 page

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ app.engine('jsx', require('express-react-views').createEngine());
 //routes
 app.use('/places', require('./controllers/places'))
 
+//home route
 app.get('/', (req, res) =>{
     res.render('home');
 })
 
+//404 error route
 app.get('*', (req, res) => {
-    res.status(404).send(`<h1>404 Page</h1>`);
+    res.render('error404');
 })
 
 app.listen(process.env.PORT);
