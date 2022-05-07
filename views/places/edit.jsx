632f02ddca884dc46dab52bd0b65d2c1de73bfc7
Alissa <alissamak@outlook.com>
2022-05-07 14:23:06 -0400

    added show, edit and new jsx under places folder

---
+++ views/places/edit.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const Def = require('../default');
+
+function editPlace(data){
+    return(
+        <Def>
+            <main>
+                <h1>EDIT</h1>
+            </main>
+        </Def>
+    )
+};
+
+module.exports = editPlace;
---
+++ views/places/new.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const Def = require('../default');
+
+function newPlace () {
+    return(
+        <Def>
+            <main>
+                <h1>NEW</h1>
+            </main>
+        </Def>
+    )
+};
+
+module.exports = newPlace;
