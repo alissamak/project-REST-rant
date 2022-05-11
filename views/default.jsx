1948c09b63040dbbdd8cd438478b2714e620bfa0
Alissa <alissamak@outlook.com>
2022-05-11 18:52:16 -0400

    added css styling and favicon

--- views/default.jsx
+++ views/default.jsx
@@ -7,8 +7,22 @@ function Def (html) {
             <title>{html.title || 'Def'}</title>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"></link>
             <link rel="stylesheet" href="/css/style.css"></link>
+            <link rel="shortcut icon" type="image/jpg" href="/public/images/cookie_favicon.ico"></link>
         </head>
         <body>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/places">Places</a>
+                    </li>
+                    <li>
+                        <a href="/places/new">Add Place</a>
+                    </li>
+                </ul>
+            </nav>
             <div className = "container">
                 {html.children}
             </div>
