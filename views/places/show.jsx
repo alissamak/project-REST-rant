17ef0f707d1ce55fc5c86b7b8674d25939f18c3c
Alissa <alissamak@outlook.com>
2022-05-11 19:03:30 -0400

    added in places index

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,14 +1,20 @@
 const React = require('react');
 const Def = require('../default');
 
-function showPlace(data){
+function show (data){
     return(
         <Def>
             <main>
-                <h1>SHOW</h1>
+                <h1>{data.place.name}</h1>
+                <h2>Rating</h2>
+                <h2>Description</h2>
+                <h2>Comments</h2>
             </main>
+            <form>
+
+            </form>
         </Def>
     )
 };
 
-module.exports = showPlace;
+module.exports = show;
