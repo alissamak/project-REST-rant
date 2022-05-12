8074cbdb779826a2feaa66b07bd761b48fcd2c4c
Alissa <alissamak@outlook.com>
2022-05-11 20:42:19 -0400

    added bootstrap css to nav

--- views/default.jsx
+++ views/default.jsx
@@ -10,22 +10,32 @@ function Def (html) {
             <link rel="shortcut icon" type="image/jpg" href="/public/images/cookie_favicon.ico"></link>
         </head>
         <body>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="/">Home</a>
-                    </li>
-                    <li>
-                        <a href="/places">Places</a>
-                    </li>
-                    <li>
-                        <a href="/places/new">Add Place</a>
-                    </li>
-                </ul>
+            <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    {/* <a className="navbar-brand" href="/">REST-Rant</a> */}
+                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/places">Places</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/places/new">New Places</a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
+
             <div className = "container">
                 {html.children}
             </div>
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
         </body>
         </html>
     )
